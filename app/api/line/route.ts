0c301eba3ab9c661ae0976c3c978b55ac40ac820
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-05-04 16:29:08 +0700

    feat(quick-reply): initial quick reply info

--- app/api/line/route.ts
+++ app/api/line/route.ts
@@ -1,3 +1,19 @@
+import reply from "@/utils/line/reply";
+
 export async function GET(request: Request) {
   return new Response("PlaCarp Line API !");
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const event = body.events[0];
+  const replyToken = event.replyToken;
+
+  console.log("event --> ", event); // for debug
+
+  if (event.type === "message" && event.message.text === "ปลาคาร์ป") {
+    await reply(replyToken, event.message.text);
+  }
+
+  return new Response("PlaCarp POST");
 }
---
+++ utils/line/reply.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
+export default async function reply(replyToken: string, msg: string) {
+  const config = {
+    headers: { "Content-Type": "application/json", Authorization: `Bearer {${channelAccessToken}}` }
+  };
+
+  const body = {
+    replyToken,
+    message: [
+      {
+        type: "text",
+        text: "ปลาคาร์ปมาแล้วจ้าาา!\nมาๆ เดี๋ยวน้องช่วยเก็บเงินให้",
+        quickReply: {
+          items: [
+            {
+              type: "action",
+              imageUrl:
+                "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683191714/line/placarp/images/thai-baht_ix7g3k.png",
+              action: {
+                type: "message",
+                label: "สร้างบิล",
+                text: "#สร้างบิล"
+              }
+            },
+            {
+              type: "action",
+              imageUrl:
+                "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683191727/line/placarp/images/more_h2rpnp.png",
+              action: {
+                type: "message",
+                label: "เพิ่มเติม",
+                text: "#เพิ่มเติม"
+              }
+            },
+            {
+              type: "action",
+              action: {
+                type: "location",
+                label: "Send location"
+              }
+            }
+          ]
+        }
+      }
+    ]
+  };
+  await axios.post("https://api.line.me/v2/bot/message/reply", body, config);
+}
