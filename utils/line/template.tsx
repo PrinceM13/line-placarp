a1192ceaf1a8945757b216c0f433e220ee33ae23
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-05-05 14:31:03 +0700

    debug: use normal msg for postback

--- utils/line/template.tsx
+++ utils/line/template.tsx
@@ -11,39 +11,43 @@ export async function ImageCarousel(replyToken: string, action: string) {
     replyToken,
     messages: [
       {
-        type: "template",
-        altText: "this is a image carousel template",
-        template: {
-          type: "image_carousel",
-          columns: [
-            {
-              imageUrl:
-                "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271051/line/placarp/images/85294_o02wt2.jpg",
-              action: {
-                type: "uri",
-                label: "chill"
-              }
-            },
-            {
-              imageUrl:
-                "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271045/line/placarp/images/OLBKWT0_sma1hb.jpg",
-              action: {
-                type: "uri",
-                label: "sakura"
-              }
-            },
-            {
-              imageUrl:
-                "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271044/line/placarp/images/Scene...8_eq5p7w.jpg",
-              action: {
-                type: "uri",
-                label: "fuji",
-                uri: "https://github.com/PrinceM13"
-              }
-            }
-          ]
-        }
+        type: "text",
+        text: "Hello, user"
       }
+      //   {
+      //     type: "template",
+      //     altText: "this is a image carousel template",
+      //     template: {
+      //       type: "image_carousel",
+      //       columns: [
+      //         {
+      //           imageUrl:
+      //             "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271051/line/placarp/images/85294_o02wt2.jpg",
+      //           action: {
+      //             type: "uri",
+      //             label: "chill"
+      //           }
+      //         },
+      //         {
+      //           imageUrl:
+      //             "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271045/line/placarp/images/OLBKWT0_sma1hb.jpg",
+      //           action: {
+      //             type: "uri",
+      //             label: "sakura"
+      //           }
+      //         },
+      //         {
+      //           imageUrl:
+      //             "https://res.cloudinary.com/dhr35jlbz/image/upload/v1683271044/line/placarp/images/Scene...8_eq5p7w.jpg",
+      //           action: {
+      //             type: "uri",
+      //             label: "fuji",
+      //             uri: "https://github.com/PrinceM13"
+      //           }
+      //         }
+      //       ]
+      //     }
+      //   }
     ]
   };
 
